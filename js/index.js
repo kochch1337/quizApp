83a51c9a22c4c5e6414c1f4ae36e0b5d604962e6
Christian Koch <christian.koch@mail.schwarz>
2022-10-07 12:13:51 +0200

    add answer button functionality

--- js/index.js
+++ js/index.js
@@ -1,7 +1,24 @@
 console.clear();
 
 const bookmarkButton = document.querySelector('[data-js="card__bookmark"]');
+const answerButton = document.querySelector('[data-js="card__button-answer"]');
+const answerCard = document.querySelector('[data-js="card__answer-q1"]');
+
+const showAnswer = "show answer";
+const hideAnswer = "hide answer";
 
 bookmarkButton.addEventListener("click", (event) => {
   event.target.classList.toggle("bookmark--active");
+});
+
+answerButton.addEventListener("click", (event) => {
+  answerCard.classList.toggle("card__answer--active");
+
+  console.log(answerButton.value);
+
+  if (answerButton.textContent.toUpperCase() === hideAnswer.toUpperCase()) {
+    answerButton.textContent = showAnswer;
+  } else {
+    answerButton.textContent = hideAnswer;
+  }
 });
